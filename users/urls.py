4e94e9161fed7db5f5537b66d7c14ffff59778d4
sayanjitdas <sayanjitgit@gmail.com>
2021-10-01 02:45:57 +0530

    author profile show api done..

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRegistrationAPIView,UserDetailsAPIView
+from .views import UserRegistrationAPIView,UserDetailsAPIView,AuthorDetailAPIView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('signup/',UserRegistrationAPIView.as_view(),name='signup'),
     path('login/',obtain_auth_token,name='login'),
-    path('obtain-user-details/',UserDetailsAPIView.as_view(),name='user-details')
+    path('obtain-user-details/',UserDetailsAPIView.as_view(),name='user-details'),
+    path('obtain-user-details/<str:email>/',AuthorDetailAPIView.as_view(),name='author-details')
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework import serializers, status,permissions
 from rest_framework.authtoken.models import Token
 
@@ -53,4 +54,10 @@ class UserDetailsAPIView(APIView):
         except Exception as e:
             print(e)
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            
+
+
+class AuthorDetailAPIView(RetrieveAPIView):
+
+    queryset = USER.objects.all()
+    serializer_class = UserDetailsSerializer
+    lookup_field = 'email'
