d2e66a7079289b77d0478794d155fde83f6c2036
sayanjitdas <sayanjitgit@gmail.com>
2021-10-02 07:09:29 -0700

    debug to false on production..

--- blogifyproject/urls.py
+++ blogifyproject/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('api/v1/',include('blog.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
